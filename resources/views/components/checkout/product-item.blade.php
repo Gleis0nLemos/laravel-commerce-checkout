f8cd567c45c13d0e1e014c69555129018354a366
Gleison Lemos <gleison04lemos@gmail.com>
2023-09-13 00:48:45 -0300

    style: Getting components ready

---
+++ resources/views/components/checkout/product-item.blade.php
@@ -0,0 +1,14 @@
+<li class="flex items-start space-x-4 py-6">
+    <img 
+        class="h-20 w-20 flex-none rounded-md object-cover object-center" 
+        src="https://tailwindui.com/img/ecommerce-images/checkout-page-07-product-01.jpg" 
+        alt="IMAGEM DO {{ $name }}" 
+    >
+    <div class="flex-auto  space-y-1">
+        <h3 class="text-white">{{ $name }}</h3>
+        @foreach ($features as $feature )
+            <p class="text-primary-200">{{ $feature }}</p>    
+        @endforeach
+    </div>
+    <p class="text-none text-base font-medium text-secondary-300">{{ $price }}</p>
+</li>
